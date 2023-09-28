761d4c0f0726390d76fbd823f7f80216fda29c6d
310639Elaf <agrimohamad@gmail.com>
2023-09-28 02:24:48 +0200

    unit test testing

--- Backend/rest.demo/src/test/java/com/waff/rest/demo/ApplicationTests.java
+++ Backend/rest.demo/src/test/java/com/waff/rest/demo/ApplicationTests.java
@@ -3,6 +3,8 @@ package com.waff.rest.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//test runnig good
+
 @SpringBootTest
 class ApplicationTests {
 

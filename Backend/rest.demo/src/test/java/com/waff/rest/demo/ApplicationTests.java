1722e36046a0d88f798d2aa9eeaef70c467ba3b4
310639Elaf <agrimohamad@gmail.com>
2023-09-28 04:36:25 +0200

    unitTest

--- Backend/rest.demo/src/test/java/com/waff/rest/demo/ApplicationTests.java
+++ Backend/rest.demo/src/test/java/com/waff/rest/demo/ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.waff.rest.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-class ApplicationTests {
+class MyServiceTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void testAddition() {
+        MyService myService = new MyService();
+        assertEquals(5, myService.addition(2, 3), "2 + 3 should equal 5");
+    }
 }
---
+++ Backend/rest.demo/src/test/java/com/waff/rest/demo/MyService.java
@@ -0,0 +1,8 @@
+package com.waff.rest.demo;
+
+public class MyService {
+    
+    public int addition(int a, int b) {
+        return a + b;
+    }
+}

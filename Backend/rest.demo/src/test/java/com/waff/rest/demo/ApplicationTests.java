a95bd8d08578b5ecae1dd22d2a6abd2b00bdab79
GitMichaGit <mbeyer98@yahoo.com>
2023-09-29 09:04:11 +0200

    Unit-Test

--- Backend/rest.demo/src/test/java/com/waff/rest/demo/ApplicationTests.java
+++ Backend/rest.demo/src/test/java/com/waff/rest/demo/ApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource("/applicationtest.properties")
+//@TestPropertySource("/applicationtest.properties")
 class ApplicationTests {
 
 	@Test
